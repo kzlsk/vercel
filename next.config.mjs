f91722120525222b7bdac020fbaa6bd8155aab1b
kzlsk <rlaflsk4887@gmail.com>
2024-08-26 04:15:23 +0900

    vercel 배포 연습

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/products/:id",
+        destination: "/items/:id",
+        permanent: true,
+      },
+    ];
+  },
+  //우리가 사용할 외부의 이미지 주소를 미리 넥스트 서버에 알려주는 것
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        //도메인을 포함한 앞쪽에 있는 주소 적기
+        hostname: "learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        // ** : 뒤쪽에 있는 모든 경로 포함
+        pathname: "/codeitmall/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
